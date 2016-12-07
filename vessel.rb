fef0837df07fc7b7b637b62f1d7d8708c05d7049
Devine Lu Linvega <aliceffekt@gmail.com>
2016-12-06 21:58:53 -0700

    Using the Dictionaery Translate method.

--- vessel.rb
+++ vessel.rb
@@ -13,98 +13,13 @@ class VesselLietal
     @path = File.expand_path(File.join(File.dirname(__FILE__), "/"))
     @docs = "The lietal language toolchain."
 
+    load_folder("#{@path}/actions/*")
+    
     install(:custom,:translate)
     install(:custom,:serve)
     install(:custom,:print)
     install(:default,:document)
     install(:default,:help)
-
-  end
-
-end
-
-class ActionServe
-
-  include Action
-
-  def initialize q = nil
-
-    super
-
-    @name = "Serve"
-    @docs = "Deliver html documentation."
-
-  end
-  
-  def act q = ""
-
-    load_folder("#{@host.path}/objects/*")
-
-    docs = Documentation.new(@host.path)
-    dict = Dictionaery.new(@host.path)
-
-    docs.dictionaery = dict
-
-    return docs.to_s
-
-  end
-
-end
-
-class ActionPrint
-
-  include Action
-
-  def initialize q = nil
-
-    super
-
-    @name = "Print"
-    @docs = "List Dictionaery content as JSON."
-
-  end
-  
-  def act q = ""
-
-    load_folder("#{@host.path}/objects/*")
-
-    d = Memory_Hash.new("dictionaery",@host.path).to_h
-    d.each do |aeth,content|
-      d[aeth]['ADL'] = Aeth.new(aeth).adultspeak
-    end
-
-    require 'json'
-
-    return d.to_json
-
-  end
-
-end
-
-class ActionTranslate
-
-  include Action
-
-  def initialize q = nil
-
-    super
-
-    @name = "Translate"
-    @docs = "Translate English word into Lietal."
-
-  end
-  
-  def act q = ""
-    
-    load_folder("#{@host.path}/objects/*")
-  
-    Memory_Hash.new("dictionaery",@host.path).to_h.each do |aeth,content|
-      if !content['DEF'] then next end
-      if !content['DEF'].like(q) then next end
-      return "#{Aeth.new(aeth).to_s}, or #{Aeth.new(aeth).adultspeak}."
-    end
-    
-    return "Unknown word."
 
   end
 
